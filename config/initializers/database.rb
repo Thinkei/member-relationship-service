4c3e04dd10c90a53ab8c6c054a4b16a60a3da4c2
Tan Nguyen <tan.nguyen@employmenthero.com>
2020-03-04 10:08:23 +0700

    refactor

--- config/initializers/database.rb
+++ config/initializers/database.rb
@@ -14,19 +14,22 @@ DB_CONFIG = db_config.freeze
 ENV['DATABASE_NAME'] ||= 'member_relationship_service'
 db_name = ENV['DATABASE_NAME']
 
+if ENV['APP_ENV'] == 'development'
+  Sequel.connect(DB_CONFIG) do |db|
+    res = db.execute 'SELECT 1 AS result FROM pg_database '\
+      "WHERE datname='#{db_name}'"
+    db.execute "CREATE DATABASE #{db_name}" if res != 1
+  end
+
+  DB = Sequel.connect(DB_CONFIG.merge(database: db_name))
+end
+
 if ENV['DATABASE_URL']
   DB = Sequel.connect(
     ENV['DATABASE_URL'],
     max_connections: (ENV['DATABASE_POOL'] || 4).to_i
   )
 else
-  if ENV['APP_ENV'] == 'development'
-    Sequel.connect(DB_CONFIG) do |db|
-      res = db.execute 'SELECT 1 AS result FROM pg_database '\
-        "WHERE datname='#{db_name}'"
-      db.execute "CREATE DATABASE #{db_name}" if res != 1
-    end
-  end
-
   DB = Sequel.connect(DB_CONFIG.merge(database: db_name))
 end
+
