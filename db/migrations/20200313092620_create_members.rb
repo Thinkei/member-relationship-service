e5d937ce90217434e058cb8c62af20f1bd046a34
Tan Nguyen <tan.nguyen@employmenthero.com>
2020-03-13 09:39:31 +0700

    add create member migrration and update schema

---
+++ db/migrations/20200313092620_create_members.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+Sequel.migration do
+  up do
+    create_table :members do
+      primary_key :id, :uuid, default: Sequel.function(:uuid_generate_v4)
+      column :organisation_id, :uuid, null: false
+      column :active, TrueClass, default: true
+      column :accepted, TrueClass, default: false
+      column :independent_contractor, TrueClass, default: false
+      column :role, String, default: 'employee'
+      column :created_at, Time, default: Sequel.function(:now)
+      column :updated_at, Time, default: Sequel.function(:now)
+    end
+  end
+
+  down do
+    drop_table :members
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -1,5 +1,18 @@
 Sequel.migration do
   change do
+    create_table(:members) do
+      column :id, "uuid", :default=>Sequel::LiteralString.new("uuid_generate_v4()"), :null=>false
+      column :organisation_id, "uuid", :null=>false
+      column :active, "boolean", :default=>true
+      column :accepted, "boolean", :default=>false
+      column :independent_contractor, "boolean", :default=>false
+      column :role, "text", :default=>"employee"
+      column :created_at, "timestamp without time zone", :default=>Sequel::CURRENT_TIMESTAMP
+      column :updated_at, "timestamp without time zone", :default=>Sequel::CURRENT_TIMESTAMP
+      
+      primary_key [:id]
+    end
+    
     create_table(:organisations) do
       column :id, "uuid", :default=>Sequel::LiteralString.new("uuid_generate_v4()"), :null=>false
       column :name, "text", :null=>false
