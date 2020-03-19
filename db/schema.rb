252d80cb467a82a6790c08f218fe0f1157a37e51
Tan Nguyen Van <40350068+TanNguyenEH@users.noreply.github.com>
2020-03-19 09:15:49 +0700

    Feat: add team model (#6)
    * add team model
    
    * add groups_members table
    
    * add groups_teams table
    
    * add members_teams table

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,22 @@ Sequel.migration do
       primary_key [:id]
     end
     
+    create_table(:groups_members) do
+      column :id, "uuid", :default=>Sequel::LiteralString.new("uuid_generate_v4()"), :null=>false
+      column :group_id, "uuid", :null=>false
+      column :member_id, "uuid", :null=>false
+      
+      primary_key [:id]
+    end
+    
+    create_table(:groups_teams) do
+      column :id, "uuid", :default=>Sequel::LiteralString.new("uuid_generate_v4()"), :null=>false
+      column :group_id, "uuid", :null=>false
+      column :team_id, "uuid", :null=>false
+      
+      primary_key [:id]
+    end
+    
     create_table(:members) do
       column :id, "uuid", :default=>Sequel::LiteralString.new("uuid_generate_v4()"), :null=>false
       column :organisation_id, "uuid", :null=>false
@@ -19,6 +35,14 @@ Sequel.migration do
       column :role, "text", :default=>"employee"
       column :created_at, "timestamp without time zone", :default=>Sequel::CURRENT_TIMESTAMP
       column :updated_at, "timestamp without time zone", :default=>Sequel::CURRENT_TIMESTAMP
+      
+      primary_key [:id]
+    end
+    
+    create_table(:members_teams) do
+      column :id, "uuid", :default=>Sequel::LiteralString.new("uuid_generate_v4()"), :null=>false
+      column :member_id, "uuid", :null=>false
+      column :team_id, "uuid", :null=>false
       
       primary_key [:id]
     end
@@ -36,6 +60,17 @@ Sequel.migration do
       column :filename, "text", :null=>false
       
       primary_key [:filename]
+    end
+    
+    create_table(:teams) do
+      column :id, "uuid", :default=>Sequel::LiteralString.new("uuid_generate_v4()"), :null=>false
+      column :name, "text"
+      column :organisation_id, "uuid", :null=>false
+      column :leader_id, "uuid"
+      column :created_at, "timestamp without time zone", :default=>Sequel::CURRENT_TIMESTAMP
+      column :updated_at, "timestamp without time zone", :default=>Sequel::CURRENT_TIMESTAMP
+      
+      primary_key [:id]
     end
   end
 end
